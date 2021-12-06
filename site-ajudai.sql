7637a762848825741557d65ca0a03f7ca7b72dd0
Crystroll <95323685+Crystroll@users.noreply.github.com>
2021-12-06 11:04:44 -0300

    Update site-ajudai.sql

--- site-ajudai.sql
+++ site-ajudai.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2021 at 05:30 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.4.25
+-- Tempo de geração: 06-Dez-2021 às 15:01
+-- Versão do servidor: 10.4.21-MariaDB
+-- versão do PHP: 7.4.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `site-ajudai`
+-- Banco de dados: `site-ajudai`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tipo-servico`
+-- Estrutura da tabela `tipo-servico`
 --
 
 CREATE TABLE `tipo-servico` (
@@ -38,7 +38,7 @@ CREATE TABLE `tipo-servico` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `trabalho`
+-- Estrutura da tabela `trabalho`
 --
 
 CREATE TABLE `trabalho` (
@@ -52,18 +52,19 @@ CREATE TABLE `trabalho` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `trabalho`
+-- Extraindo dados da tabela `trabalho`
 --
 
 INSERT INTO `trabalho` (`Id`, `nome`, `descricao`, `tipo_servico`, `Id_usuario`, `data_criacao`, `thumb`) VALUES
 (27, 'Exemplo de trabalho 1', 'Aqui o usuário coloca um descrição simples de seu trabalho.', 1, 0, '2021-12-06 03:13:36', '../TCC/images_products/e2601e1456ca8a66d54deee28533e04d.png'),
 (28, 'Pedro Pinturas', 'Eu sei pintar paredes muito bem!\r\nUm dos melhores preços da cidade.', 1, 0, '2021-12-06 03:15:23', '../TCC/images_products/e376d7e8f355cf988e243bde14ccd773.png'),
-(29, 'Pedreiro Paulo', 'Qualquer tipo de construção é comigo!', 1, 0, '2021-12-06 03:17:53', '../TCC/images_products/223c46edf46708e2261896fb694349de.png');
+(29, 'Pedreiro Paulo', 'Qualquer tipo de construção é comigo!', 1, 0, '2021-12-06 03:17:53', '../TCC/images_products/223c46edf46708e2261896fb694349de.png'),
+(30, 'trab1', 'tra1 desc', 1, 0, '2021-12-06 12:39:28', '../TCC/images_products/489f744ffe0051bef7e88ce9cda8a18f.png');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuario`
+-- Estrutura da tabela `usuario`
 --
 
 CREATE TABLE `usuario` (
@@ -76,7 +77,7 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `usuario`
+-- Extraindo dados da tabela `usuario`
 --
 
 INSERT INTO `usuario` (`Id`, `nome`, `email`, `senha`, `telefone`, `pix`) VALUES
@@ -86,51 +87,53 @@ INSERT INTO `usuario` (`Id`, `nome`, `email`, `senha`, `telefone`, `pix`) VALUES
 (4, '1111111', '1111@g.com', '7fa8282ad93047a4d6fe6111c93b308a', 11111, NULL),
 (5, 'Raquel Rubio de Souza ', '1234@gmail.com', 'd41d8cd98f00b204e9800998ecf8427e', 123, NULL),
 (6, 'ladno', '123@g', 'd41d8cd98f00b204e9800998ecf8427e', 123, NULL),
-(7, '11', '1234444@gmail.com', 'd41d8cd98f00b204e9800998ecf8427e', 123, NULL);
+(7, '11', '1234444@gmail.com', 'd41d8cd98f00b204e9800998ecf8427e', 123, NULL),
+(8, 'cadas', '123123@g', 'd41d8cd98f00b204e9800998ecf8427e', 123, NULL),
+(9, 'cadas2', '1234@g', 'd41d8cd98f00b204e9800998ecf8427e', 0, NULL);
 
 --
--- Indexes for dumped tables
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `tipo-servico`
+-- Índices para tabela `tipo-servico`
 --
 ALTER TABLE `tipo-servico`
   ADD PRIMARY KEY (`Id`);
 
 --
--- Indexes for table `trabalho`
+-- Índices para tabela `trabalho`
 --
 ALTER TABLE `trabalho`
   ADD PRIMARY KEY (`Id`);
 
 --
--- Indexes for table `usuario`
+-- Índices para tabela `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`Id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `tipo-servico`
+-- AUTO_INCREMENT de tabela `tipo-servico`
 --
 ALTER TABLE `tipo-servico`
   MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `trabalho`
+-- AUTO_INCREMENT de tabela `trabalho`
 --
 ALTER TABLE `trabalho`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
--- AUTO_INCREMENT for table `usuario`
+-- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
