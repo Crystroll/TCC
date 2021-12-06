87aa8007aea46314e898abc3dce1747938788732
Crystroll <95323685+Crystroll@users.noreply.github.com>
2021-12-06 12:32:44 -0300

    cadastro

--- registro.php
+++ registro.php
@@ -16,30 +16,32 @@ include "header.php";
             <div class="col-5">
 
                 <div class="cadastro">
-                    <h2 class="text-center"> Faça Seu Cadastro </h2>
+                    <h2 class="text-center"> Realize Seu Cadastro </h2>
 
                     <form action="operador_registro.php" method="post" name="form-cadastro">
-                        <div class="mb-3">
+                        <div class="my-4">
                             <label for="email" class="form-label">Nome</label>
                             <input type="text" class="form-control " id="nome" name="nome" placeholder="Digite o seu nome">
                         </div>   
-                        <div class="mb-3">
+                        <div class="my-4">
                             <label for="email" class="form-label">E-mail</label>
                             <input type="email" class="form-control " id="email" name="email" placeholder="Digite o e-mail">
                         </div>   
-                        <div class="mb-3">
+                        <div class="my-4">
                             <label for="email" class="form-label">Telefone</label>
                             <input type="tel" class="form-control" name="telefone"  id="telefone" placeholder="Digite o e-mail">
                         </div>   
-                        <div class="mb-3">
+                        <div class="my-4">
                             <label for="senha" class="form-label">Senha</label>
                             <input type="password" class="form-control" id="password" name="password" placeholder="Senha">
                         </div>     
-                        <div class="buttons">
+                        <div class="buttons my-2" >
                             <button class="btn btn-primary"> Cadastrar </button>
                         </div>
                     </form>  
-                    <a href="" class="text-center" id="login">Faça seu Login</a> 
+                    Já possui conta? <a href="login.php" class="text-center" id="login">Faça login</a> 
+
+                    <br /><br /><br /><br /><br /><br />
                 </div>
             </div>
         </div>
