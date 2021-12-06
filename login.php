91d75335696a60728fc3e0de18ee1a2278252703
Crystroll <95323685+Crystroll@users.noreply.github.com>
2021-12-06 09:27:45 -0300

    t1

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+
 <?php
 include "header.php";
 
@@ -17,15 +18,7 @@ if((isset ($_SESSION['login']) == true) and (isset ($_SESSION['senha']) == true)
     <link rel='stylesheet' type='text/css' media='screen' href='css/style.css'>
 </head>
 <body>
-<style>
-.login{
-    display:block;
-}
 
-.cadastro{
-    display:none;
-}
-</style>
     <div class="container">
         <div class="row d-flex justify-content-center align-items-center mt-5">
             <div class="col-5">
@@ -34,16 +27,15 @@ if((isset ($_SESSION['login']) == true) and (isset ($_SESSION['senha']) == true)
                     <h2 class="text-center"> Faça seu Login </h2>
                         <form action="operador_login.php">
                             <div class="mb-3">
-                                <label for="email" class="form-label">E-mail</label>
-                                <input type="email" class="form-control " id="email" name="email" placeholder="Digite o e-mail">
+                                <label for="email" class="row justify-content-center">Email</label>
+                                <input type="text" name="email" class='mb-2'/>
                             </div>   
                             <div class="mb-3">
                                 <label for="senha" class="form-label">Senha</label>
                                 <input type="password" class="form-control" id="password" name="password" placeholder="Senha">
                             </div>     
                             <div class="buttons">
-                                <button class="btn btn-primary"> Acessar </button>
-                                <button type="submit" class="btn btn-primary mt-3" name="login" value="Entrar">Entrar</button>
+                                    <button type="submit" class="btn btn-primary mt-3" name="login" value="Entrar">Entrar</button>
                             </div>
                         </form>  
                     <a href="#" class="text-center" id="cadastro">Faça seu cadastro</a> 
@@ -54,26 +46,6 @@ if((isset ($_SESSION['login']) == true) and (isset ($_SESSION['senha']) == true)
             </div>
         </div>   
     </div>    
-<script>
-var cad = document.getElementById('cadastro');
-var log = document.getElementById('login');
-var login = document.querySelector('.login');
-var cadastro = document.querySelector('.cadastro');
-
-
-cad.addEventListener('click', function(){
-console.log("Clique");
-login.style.display="none";
-cadastro.style.display="block";
-})
-
-log.addEventListener('click', function(){
-    cadastro.style.display="none";
-    login.style.display="block";
-})
-</script>    
-
-
     
 </body>
 </html>
